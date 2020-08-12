3d9ca0fb89fd27b78ba6e40a7b69989431010c2f
Thomas Burton <twyburton@outlook.com>
2020-08-12 19:03:39 +0100

    Added some basic interface

---
+++ covid/main.js
@@ -0,0 +1,6 @@
+if ('mediaDevices' in navigator && 'getUserMedia' in navigator.mediaDevices) {
+    console.log("Let's get this party started")
+
+    navigator.mediaDevices.getUserMedia({video: true})
+
+}
