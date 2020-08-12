1af8109b71d3b66a94ea4d4432911ab61c30624f
Thomas Burton <twyburton@outlook.com>
2020-08-12 19:40:04 +0100

    Added first video code

--- covid/main.js
+++ covid/main.js
@@ -1,6 +1,83 @@
 if ('mediaDevices' in navigator && 'getUserMedia' in navigator.mediaDevices) {
     console.log("Let's get this party started")
-
-    navigator.mediaDevices.getUserMedia({video: true})
-
 }
+
+navigator.mediaDevices.getUserMedia({video: true})
+
+
+
+let streamStarted = false;
+const video = document.querySelector('video');
+const cameraOptions = document.querySelector('.video-options>select');
+
+const constraints = {
+    video: {
+        width: {
+            min: 600,
+            ideal: 1920,
+            max: 2560,
+        },
+        height: {
+            min: 300,
+            ideal: 1080,
+            max: 1440
+        }
+    }
+};
+
+
+const getCameraSelection = async () => {
+    const devices = await navigator.mediaDevices.enumerateDevices();
+    const videoDevices = devices.filter(device => device.kind === 'videoinput');
+    const options = videoDevices.map(videoDevice => {
+        return `<option value="${videoDevice.deviceId}">${videoDevice.label}</option>`;
+    });
+    cameraOptions.innerHTML = options.join('');
+};
+
+playStream = () => {
+    if (streamStarted) {
+        video.play();
+        play.classList.add('d-none');
+        pause.classList.remove('d-none');
+        return;
+    }
+    if ('mediaDevices' in navigator && navigator.mediaDevices.getUserMedia) {
+        const updatedConstraints = {
+            ...constraints,
+            deviceId: {
+            exact: cameraOptions.value
+            }
+        };
+        startStream(updatedConstraints);
+    }
+};
+
+const startStream = async (constraints) => {
+    const stream = await navigator.mediaDevices.getUserMedia(constraints);
+    handleStream(stream);
+};
+
+const handleStream = (stream) => {
+    video.srcObject = stream;
+    streamStarted = true;
+};
+
+getCameraSelection();
+
+cameraOptions.onchange = () => {
+  const updatedConstraints = {
+    ...constraints,
+    deviceId: {
+      exact: cameraOptions.value
+    }
+  };
+  startStream(updatedConstraints);
+};
+
+const pauseStream = () => {
+    video.pause();
+};
+
+
+playStream();
